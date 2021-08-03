558411fa3a9afa26532a7fd1c0c8b45bfbfc3b2a
Cleberson Gilvan <34071409+Cleber4@users.noreply.github.com>
2021-08-03 17:17:22 -0300

    [#01] - Domain and test

---
+++ lib/features/domain/repositories/space_media_repository.dart
@@ -0,0 +1,8 @@
+import 'package:app_nasa/core/errors/failures.dart';
+import 'package:app_nasa/features/domain/entities/space_media_entity.dart';
+import 'package:dartz/dartz.dart';
+
+abstract class ISpaceMediaRepository {
+  Future<Either<Failure, SpaceMediaEntity>> getSpaceMediaFromDate(
+      DateTime date);
+}
